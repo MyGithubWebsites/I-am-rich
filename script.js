350dc14020e1f7d72d1be8445a90c050bc5b3ec1
MyGithubWebsites <157920715+MyGithubWebsites@users.noreply.github.com>
2024-05-06 12:10:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+  document.querySelector('h1').textContent = 'I am rich. I deserve it. I am good, healthy & successful';
+});
